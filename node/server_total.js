8933b4dacd25eb718f12cf62943189986fbdc4d7
pluckychuang <pluckychuang@163.com>
2019-03-11 23:52:32 +0800

    c

--- node/server_total.js
+++ node/server_total.js
@@ -1,13 +1,14 @@
 let http = require('http')
 let url = require('url')
 let querystring = require('querystring')
+let fs = require('fs')
 
 let users = {}
 
 http.createServer((req, res) => {
   let path = '', get = {}, post = {}
   if (req.method === 'GET') {
-    let {pathname, query} = url.parse(req.url, ture)
+    let {pathname, query} = url.parse(req.url, true)
 
     path = pathname
     get = query
@@ -20,30 +21,49 @@ http.createServer((req, res) => {
       arr.push(buffer)
     })
     req.on('end', () => {
-      let buffer = Bffer.concat(arr)
+      let buffer = Buffer.concat(arr)
 
       post = querystring.parse(buffer.toString())
       complete()
     })
   }
+  console.log(users)
 
   function complete() {
-    if (path == '/reg') {
-      let {username, password} = get
+    let {username, password} = get
 
+    if (path == '/reg') {
       if (users[username]) {
-        res.write({error: 1, msg: ''})
+        res.write(JSON.stringify({error: 1, msg: '此用户已存在'}))
         res.end()
       } else {
         users[username] = password
 
-        res.write({error: 0, msg: ''})
+        res.write(JSON.stringify({error: 0, msg: ''}))
         res.end()
       }
     } else if (path == '/login') {
-
+      if (!users[username]) {
+        res.write(JSON.stringify({error: 1, msg: '此用户不存在'}))
+        res.end()
+      } else if (users[username] !== password) {
+        res.write(JSON.stringify({error: 1, msg: '账号或密码错误'}))
+        res.end()
+      } else {
+        res.write(JSON.stringify({error: 0, msg: ''}))
+        res.end()
+      }
     } else {
-
+      fs.readFile(`./${path}`, (error, buffer) => {
+        if (error) {
+          res.writeHeader(404)
+          res.write('Not Found')
+          res.end()
+        } else {
+          res.write(buffer)
+          res.end()
+        }
+      })
     }
   }
 }).listen(8080)
