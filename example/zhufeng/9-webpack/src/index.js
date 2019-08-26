ab3bf68462faa44392f498181239e6e94353708a
pluckychuang <pluckychuang@163.com>
2019-08-26 11:16:25 +0800

    jz

--- example/zhufeng/9-webpack/src/index.js
+++ example/zhufeng/9-webpack/src/index.js
@@ -1,2 +1,7 @@
-import number from './a.js'
-console.log(number)
+
+import './index.css'
+let logo = require('./img/logo.png')
+let img = new Image()
+img.src = logo
+img.style.width = '100px'
+document.body.appendChild(img)
--- example/zhufeng/9-webpack/webpack.config.js
+++ example/zhufeng/9-webpack/webpack.config.js
@@ -1,15 +1,22 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} =  require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development',
   devtool: 'eval',
+  // 单入口打包 默认chunk名为'main'，每个chunk一般会生成一个文件
   entry: path.join(__dirname, 'src/index.js'),
+  // 多入口打包
+  // entry: {
+  //   index: path.join(__dirname, 'src/index.js'), // index为chunk名字
+  //   login: path.join(__dirname, 'src/login.js')
+  // },
   output: {
     path:path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/' // 访问路径为：publicPath+outputPath
   },
   // 如果使用devServer, 那么所有产出的文件都会写入内存里，而不是硬盘
   devServer: {
@@ -18,7 +25,24 @@ module.exports = {
     host: 'localhost',
     compress: true
   },
-  // module: {},
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|gif|jpe?g|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            // 如果小于10k,就将图片转成base64，嵌入HTML中
+            limit: 10
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     // 一个插件就是一个类 (class)
     new HtmlWebpackPlugin({
@@ -28,8 +52,13 @@ module.exports = {
       chunks: ['main'],
       chunksSortMode: 'manual' // 对引入的代码块进行排序模式
     }),
+    // 在打包前清空目录
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: ['dist']
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name][hash].css', // name是代码中chunk名字
+      chunkFilename: '[id].css' // 异步加载时用到
     })
   ]
 }
