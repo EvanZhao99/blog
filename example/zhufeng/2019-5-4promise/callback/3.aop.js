c79f16a194ba63481134e38e0bab84a43617cfc9
pluckychuang <pluckychuang@163.com>
2019-11-30 20:03:21 +0800

    职业规划

---
+++ example/zhufeng/2019-5-4promise/callback/3.aop.js
@@ -0,0 +1,17 @@
+// AOP 面向切片编程 把原来的代码切成片 再中间加上自己的代码
+// 装饰器 扩展原有的方法 重写原有的方法
+function say(who) {
+    console.log(who+ '说话')
+}
+
+Function.prototype.before = function(callback) {
+    let self = this
+    return function() {
+        callback()
+        self(...arguments)
+    }
+}
+let newFn = say.before(function(){ // 新增方法
+    console.log('请问')
+})
+newFn('我')
