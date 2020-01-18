cdb036a3cc11821e7aff40d1e1c0933a98fb2e80
pluckychuang <pluckychuang@163.com>
2020-01-18 15:09:34 +0800

    算法

--- doc/test.js
+++ doc/test.js
@@ -1,21 +1,12 @@
-function canSort(current, target) {
-  let queue = []
-  let i = 0
-  while(current.length > 0) {
-    console.log(queue, current, target, i)
-    if(target[i] === queue[0]) {
-      queue.shift()
-      i++
-    } else if(target[i] === current[0]){
-      current.shift()
-      i++
-    } else {
-      queue.push(current.shift())
-    }
+function sum(x, y) {
+  if(x === 0 && y === 0) {
+    return 1
+  } else if(x > 0 && y === 0) {
+    return sum(x-1, y)
+  } else if(x === 0 && y > 0) {
+    return sum(x, y-1)
+  } else {
+    return sum(x-1,y) + sum(x, y-1)
   }
-  console.log(queue, current, target)
-  return queue.length === 0
-
 }
-
-console.log(canSort([1,2,3,4,5],[1,2,4,3,5]))
+console.log(sum(3, 4))
