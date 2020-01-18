58b03bc697434a46b0d266c9a3c8968a0e2f7f61
pluckychuang <pluckychuang@163.com>
2020-01-18 12:40:29 +0800

    车厢排序问题

--- doc/test.js
+++ doc/test.js
@@ -1,10 +1,21 @@
-function side(arr){
-      arr[0] = arr[2];
+function canSort(current, target) {
+  let queue = []
+  let i = 0
+  while(current.length > 0) {
+    console.log(queue, current, target, i)
+    if(target[i] === queue[0]) {
+      queue.shift()
+      i++
+    } else if(target[i] === current[0]){
+      current.shift()
+      i++
+    } else {
+      queue.push(current.shift())
+    }
   }
-  function a(a,b,c=3){
-      c = 10;
-      side(arguments);
-    
-      return a+b+c;
-  }
-  console.log(a(1,1,1))
+  console.log(queue, current, target)
+  return queue.length === 0
+
+}
+
+console.log(canSort([1,2,3,4,5],[1,2,4,3,5]))
