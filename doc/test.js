3fdcc8501f516387bf1989cb55ac2a812b98af43
pluckychuang <pluckychuang@163.com>
2020-02-24 23:33:36 +0800

    doc

--- doc/test.js
+++ doc/test.js
@@ -1,9 +1,28 @@
-let Koa = require('koa')
+class Queue{
+    constructor() {
+        this.tasks= []
+    }
+    add(time,data) {
+        setTimeout(() => {
+            this.publish({time, data})
+        }, new Date(time).getTime() - Date.now())
+    }
+    publish(task) {
+        this.tasks.push(task)
+    }
+    listen() {
+        setInterval(() => {
+            let index = this.tasks.length -1
+            while(index >= 0) {
+                let task = this.tasks[index--]
+                console.log(task.data, Date.now() - task.time)
+            }
+            this.tasks.length = 0
+        }, 1000)
+    }
+}
 
-let app = new Koa()
-
-app.use(async (ctx, next) => {
-    ctx.body = 'hello world'
-})
-
-app.listen(5000)
+console.log('start:', Date.now())
+let queue = new Queue()
+queue.add(Date.now() + 2398, 'hello world')
+queue.listen()
